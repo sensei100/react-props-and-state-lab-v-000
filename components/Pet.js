c3455beeefd8e366af733250d5e8ff171a048765
sensei100 <jenanton01@gmail.com>
2017-06-05 18:53:07 -0400

    pet tests passing

--- components/Pet.js
+++ components/Pet.js
@@ -3,24 +3,37 @@ const React = require('react');
 class Pet extends React.Component {
   constructor() {
     super();
+
+    this.adoptPet = this.adoptPet.bind(this);
+  }
+
+  adoptPet() {
+    this.props.onAdoptPet(this.props.pet.id)
   }
 
   render() {
+    var button;
+
+    if (!this.props.isAdopted) {
+      button = <button className="ui primary button" onClick={this.adoptPet}>Adopt Pet</button>
+    } else {
+      button = <button className="ui disabled button">Already adopted</button>
+    }
+
     return (
       <div className="card">
         <div className="content">
-          <a className="header">Pet name (gender: ♂ or ♀)</a>
+          <a className="header">{this.props.pet.name}{this.props.pet.gender === 'male' ? '♂' : '♀'}</a>
           <div className="meta">
-            <span className="date">Pet type</span>
+            <span className="date">{this.props.pet.type}</span>
           </div>
           <div className="description">
-            <p>Age: </p>
-            <p>Weight: </p>
+            <p>Age: {this.props.pet.age} </p>
+            <p>Weight: {this.props.pet.weight} </p>
           </div>
         </div>
         <div className="extra content">
-          <button className="ui primary button">Adopt pet</button>
-          <button className="ui disabled button">Already adopted</button>
+          {button}
         </div>
       </div>
     );
